fd802e05423cf6142941fbdaa2926c671bffe5a3
built-different-13 <built.different7813@gmail.com>
2025-05-10 10:41:42 +0530

    Add files via upload

---
+++ Patterns 1/pattern16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of rows" << endl;
+    cin >> n;
+    cout << "The Pattern is printed below" << endl;
+    int i=1;
+    while(i<=n) {
+        int j=1;
+        char ch = 'A'+n-i;
+        while(j<=i) {
+            cout << ch << " ";
+            ch++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
